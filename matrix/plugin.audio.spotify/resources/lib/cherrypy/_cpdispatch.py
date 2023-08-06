1b1d325d118214958376a2afd2f03985583b302a
CodingPF <j-doo@gmx.de>
2023-08-06 20:27:21 +0200

    spotify v.1.3.1

--- matrix/plugin.audio.spotify/resources/lib/cherrypy/_cpdispatch.py
+++ matrix/plugin.audio.spotify/resources/lib/cherrypy/_cpdispatch.py
@@ -206,12 +206,13 @@ except ImportError:
     def test_callable_spec(callable, args, kwargs):  # noqa: F811
         return None
 else:
-    getargspec = inspect.getargspec
     # Python 3 requires using getfullargspec if
     # keyword-only arguments are present
+    # FIX for Kodi20 > https://github.com/ldsz/plugin.audio.spotify/issues/35
     if hasattr(inspect, 'getfullargspec'):
-        def getargspec(callable):
-            return inspect.getfullargspec(callable)[:4]
+        getargspec = inspect.getfullargspec(callable)[:4]
+    else:
+        getargspec = inspect.getargspec(callable)[:4]
 
 
 class LateParamPageHandler(PageHandler):
